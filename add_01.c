488fd755ed0327097572c54cc62b507907b76666
george-eee <147610823+george-eee@users.noreply.github.com>
2023-12-17 23:47:30 +0800

    Add files via upload

--- add_01.c
+++ add_01.c
@@ -2767,295 +2767,372 @@
 //3_玩家下棋/电脑下棋 并实时判断是否有胜者 是否平局
 //4_回到菜单 是否继续？
 //我们来实现一下：
-#define Row 3
-#define Col 3
-int flag = 0;
-void menu()
-{
-	char arr1[] = "            ";
-	char arr2[] = "Play or Not?";
-	char arr3[] = "                           ";
-	char arr4[] = "Play: input 1 exit: input 2";
-	int left1 = 0, left2 = 0;
-	size_t right1 = strlen(arr1 - 1);
-	size_t right2 = strlen(arr3 - 1);
-	while (left1<=right1)
-	{
-		arr1[left1] = arr2[left1];
-		arr1[right1] = arr2[right1];
-		printf("%s\n", arr1);
-		Sleep(300);
-		system("cls");
-		left1++;
-		right1--;
-	}
-	while (left2<=right2)
-	{
-		arr3[left2] = arr4[left2];
-		arr3[right2] = arr4[right2];
-		printf("%s\n", arr3);
-		Sleep(150);
-		system("cls");
-		left2++;
-		right2--;
-	}
-	printf("%s\n%s\n", arr1,arr3);
-}
-void initboard(char chessboard[Row][Col], int row, int col)
-{
-	int i=0, j = 0;
-	for ( i = 0; i < row; i++)
-	{
-		for ( j = 0; j < col; j++)
-		{
-			chessboard[i][j] = ' ';
-		}
-	}
-}
-void printboard(char chessboard[Row][Col],int row,int col)
-{
-	int i = 0;
-	for (int i = 0; i < row; i++)
-	{
-		int j = 0;
-		for (j = 0; j < col; j++)
-		{
-			if (j < col - 1)
-			{
-				printf(" %c |", chessboard[i][j]);
-				
-			}
-			else
-				printf(" %c \n", chessboard[i][j]);
-		}
-		if (i<row-1)
-		{
-			for (int j = 0; j < col; j++)
-			{
-				if (j < col - 1)
-			{
-					printf("---|");
-			}
-				else
-					printf("---\n");
-			}
-		}
-	}
-}
-void playermove(char chessboard[Row][Col], int row, int col)
-{
-	int x, y = 0;
-	printf("Player Chess-->input n,n \n");
-	scanf("%d,%d", &x, &y);
-	while (true)
-	{
-		if (x > 0 && x <= row && y > 0 && y <= col)
-		{
-			if (chessboard[x - 1][y - 1] == ' ')
-			{
-				chessboard[x - 1][y - 1] = '*';
-				flag = 0;
-				break;
-			}
-			else
-			{
-				printf("This coordinate is already occupied,please re-enter it.\n");
-				Sleep(2500);
-				flag = 1;
-				break;
-			}
-		}
-		else
-		{
-			printf("The coordinates entered are illegal.\nPlease try again.\n");
-			Sleep(2500);
-			flag = 1;
-			break;
-		}
-	}
-}
-void computermove(char chessboard[Row][Col], int row, int col)
-{
-	printf("Computer chess...\n");
-	Sleep(2000);
-	int x, y = 0;
-	while (1)
-	{
-		x = rand() % row;
-		y = rand() % col;
-		if (chessboard[x][y] == ' ')
-		{
-			chessboard[x][y] = '#';
-			break;
-		}
-	}
-}
-char ionwin(char chessboard[Row][Col])
-{
-	int i, j = 0;
-	for ( i = 0; i < Row; i++)//行等
-	{
-		int countp=0,countc = 0;
-		for ( j = 0; j < Col; j++)
-		{
-			if (chessboard[i][j]=='*')
-			{
-				countp++;
-			}
-			if (chessboard[i][j]=='#')
-			{
-				countc++;
-			}
-		}
-		if (countp==3)
-		{
-			return 'y';
-		}
-		if (countc==3)
-		{
-			return 'n';
-		}
-	}
-	for ( j = 0; j < Col; j++)//列等
-	{
-		int countp = 0, countc = 0;
-		for ( i = 0; i < Row; i++)
-		{
-			if (chessboard[i][j] == '*')
-			{
-				countp++;
-			}
-			if (chessboard[i][j] == '#')
-			{
-				countc++;
-			}
-		}
-		if (countp == 3)
-		{
-			return 'y';
-		}
-		if (countc == 3)
-		{
-			return 'n';
-		}
-	}
-	if (1)//交叉等
-	{
-		int countp1 = 0, countc1 = 0;
-		int countp2 = 0, countc2 = 0;
-		for (i = 0, j = 0; i < Row && j < Col; i++ && j++)
-		{
-			if (chessboard[i][j] == '*')
-				countp1++;
-			else if (chessboard[i][j] == '#')
-				countc1++;
-			else
-				break;
-		}
-		for (i = 2, j = 0; i >= 0 && j < Col; i-- && j++)
-		{
-			if (chessboard[i][j] == '*')
-				countp2++;
-			else if (chessboard[i][j] == '#')
-				countc2++;
-			else
-				break;
-		}
-		if (countp1==3||countp2==3)
-		{
-			return 'y';
-		}
-		if (countc1==3||countc2==3)
-		{
-			return 'n';
-		}
-	}
-	int flag = 1;//判断平局
-	for (int i = 0; i < Row; i++)
-	{
-		for (int j = 0; j < Col; j++)
-		{
-			if (chessboard[i][j]==' ')
-			{
-				flag = 0;
-			}
-		}
-	}
-	if (flag)
-	{
-		return 't';
-	}
-}
-void game()
-{
-	char chessboard[Row][Col] = { 0 };
-	initboard(chessboard, Row, Col);
-	printboard(chessboard,Row,Col);
-	while (1)
-	{
-		char judge = 0;
-		playermove(chessboard, Row, Col);
-		printboard(chessboard, Row, Col);
-		judge=ionwin(chessboard);
-		if (judge=='y')
-		{
-			printf("YOU WIN!\n");
-			Sleep(1500);
-			break;
-		}
-		else if (judge=='t')
-		{
-			printf("STALEMATE!\n");
-			Sleep(1500);
-			break;
-		}
-		if (flag==0)
-		{
-			computermove(chessboard, Row, Col);
-			printboard(chessboard, Row, Col);
-		}
-		judge = ionwin(chessboard);
-		if (judge == 'n')
-		{
-			printf("YOU LOSE!\n");
-			Sleep(1500);
-			break;
-		}
-		else if (judge == 't')
-		{
-			printf("STALEMATE!\n");
-			Sleep(1500);
-			break;
-		}
-	}
-}
+//#define Row 3
+//#define Col 3
+//int flag = 0;
+//void menu()
+//{
+//	char arr1[] = "            ";
+//	char arr2[] = "Play or Not?";
+//	char arr3[] = "                           ";
+//	char arr4[] = "Play: input 1 exit: input 2";
+//	int left1 = 0, left2 = 0;
+//	size_t right1 = strlen(arr1 - 1);
+//	size_t right2 = strlen(arr3 - 1);
+//	while (left1<=right1)
+//	{
+//		arr1[left1] = arr2[left1];
+//		arr1[right1] = arr2[right1];
+//		printf("%s\n", arr1);
+//		Sleep(300);
+//		system("cls");
+//		left1++;
+//		right1--;
+//	}
+//	while (left2<=right2)
+//	{
+//		arr3[left2] = arr4[left2];
+//		arr3[right2] = arr4[right2];
+//		printf("%s\n", arr3);
+//		Sleep(150);
+//		system("cls");
+//		left2++;
+//		right2--;
+//	}
+//	printf("%s\n%s\n", arr1,arr3);
+//}
+//void initboard(char chessboard[Row][Col], int row, int col)
+//{
+//	int i=0, j = 0;
+//	for ( i = 0; i < row; i++)
+//	{
+//		for ( j = 0; j < col; j++)
+//		{
+//			chessboard[i][j] = ' ';
+//		}
+//	}
+//}
+//void printboard(char chessboard[Row][Col],int row,int col)
+//{
+//	int i = 0;
+//	for (int i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			if (j < col - 1)
+//			{
+//				printf(" %c |", chessboard[i][j]);
+//				
+//			}
+//			else
+//				printf(" %c \n", chessboard[i][j]);
+//		}
+//		if (i<row-1)
+//		{
+//			for (int j = 0; j < col; j++)
+//			{
+//				if (j < col - 1)
+//			{
+//					printf("---|");
+//			}
+//				else
+//					printf("---\n");
+//			}
+//		}
+//	}
+//}
+//void playermove(char chessboard[Row][Col], int row, int col)
+//{
+//	int x, y = 0;
+//	printf("Player Chess-->input n,n \n");
+//	scanf("%d,%d", &x, &y);
+//	while (true)
+//	{
+//		if (x > 0 && x <= row && y > 0 && y <= col)
+//		{
+//			if (chessboard[x - 1][y - 1] == ' ')
+//			{
+//				chessboard[x - 1][y - 1] = '*';
+//				flag = 0;
+//				break;
+//			}
+//			else
+//			{
+//				printf("This coordinate is already occupied,please re-enter it.\n");
+//				Sleep(2500);
+//				flag = 1;
+//				break;
+//			}
+//		}
+//		else
+//		{
+//			printf("The coordinates entered are illegal.\nPlease try again.\n");
+//			Sleep(2500);
+//			flag = 1;
+//			break;
+//		}
+//	}
+//}
+//void computermove(char chessboard[Row][Col], int row, int col)
+//{
+//	printf("Computer chess...\n");
+//	Sleep(2000);
+//	int x, y = 0;
+//	while (1)
+//	{
+//		x = rand() % row;
+//		y = rand() % col;
+//		if (chessboard[x][y] == ' ')
+//		{
+//			chessboard[x][y] = '#';
+//			break;
+//		}
+//	}
+//}
+//char ionwin(char chessboard[Row][Col])
+//{
+//	int i, j = 0;
+//	for ( i = 0; i < Row; i++)//行等
+//	{
+//		int countp=0,countc = 0;
+//		for ( j = 0; j < Col; j++)
+//		{
+//			if (chessboard[i][j]=='*')
+//			{
+//				countp++;
+//			}
+//			if (chessboard[i][j]=='#')
+//			{
+//				countc++;
+//			}
+//		}
+//		if (countp==3)
+//		{
+//			return 'y';
+//		}
+//		if (countc==3)
+//		{
+//			return 'n';
+//		}
+//	}
+//	for ( j = 0; j < Col; j++)//列等
+//	{
+//		int countp = 0, countc = 0;
+//		for ( i = 0; i < Row; i++)
+//		{
+//			if (chessboard[i][j] == '*')
+//			{
+//				countp++;
+//			}
+//			if (chessboard[i][j] == '#')
+//			{
+//				countc++;
+//			}
+//		}
+//		if (countp == 3)
+//		{
+//			return 'y';
+//		}
+//		if (countc == 3)
+//		{
+//			return 'n';
+//		}
+//	}
+//	if (1)//交叉等
+//	{
+//		int countp1 = 0, countc1 = 0;
+//		int countp2 = 0, countc2 = 0;
+//		for (i = 0, j = 0; i < Row && j < Col; i++ && j++)
+//		{
+//			if (chessboard[i][j] == '*')
+//				countp1++;
+//			else if (chessboard[i][j] == '#')
+//				countc1++;
+//			else
+//				break;
+//		}
+//		for (i = 2, j = 0; i >= 0 && j < Col; i-- && j++)
+//		{
+//			if (chessboard[i][j] == '*')
+//				countp2++;
+//			else if (chessboard[i][j] == '#')
+//				countc2++;
+//			else
+//				break;
+//		}
+//		if (countp1==3||countp2==3)
+//		{
+//			return 'y';
+//		}
+//		if (countc1==3||countc2==3)
+//		{
+//			return 'n';
+//		}
+//	}
+//	int flag = 1;//判断平局
+//	for (int i = 0; i < Row; i++)
+//	{
+//		for (int j = 0; j < Col; j++)
+//		{
+//			if (chessboard[i][j]==' ')
+//			{
+//				flag = 0;
+//			}
+//		}
+//	}
+//	if (flag)
+//	{
+//		return 't';
+//	}
+//}
+//void game()
+//{
+//	char chessboard[Row][Col] = { 0 };
+//	initboard(chessboard, Row, Col);
+//	printboard(chessboard,Row,Col);
+//	while (1)
+//	{
+//		char judge = 0;
+//		playermove(chessboard, Row, Col);
+//		printboard(chessboard, Row, Col);
+//		judge=ionwin(chessboard);
+//		if (judge=='y')
+//		{
+//			printf("YOU WIN!\n");
+//			Sleep(1500);
+//			break;
+//		}
+//		else if (judge=='t')
+//		{
+//			printf("STALEMATE!\n");
+//			Sleep(1500);
+//			break;
+//		}
+//		if (flag==0)
+//		{
+//			computermove(chessboard, Row, Col);
+//			printboard(chessboard, Row, Col);
+//		}
+//		judge = ionwin(chessboard);
+//		if (judge == 'n')
+//		{
+//			printf("YOU LOSE!\n");
+//			Sleep(1500);
+//			break;
+//		}
+//		else if (judge == 't')
+//		{
+//			printf("STALEMATE!\n");
+//			Sleep(1500);
+//			break;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	srand((unsigned int)time(NULL));
+//	do
+//	{
+//		menu();
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			printf("Enter the game...\n");
+//			Sleep(1000);
+//			game();
+//			break;
+//		case 2:
+//			printf("Exit the game...\n");
+//			Sleep(1000);
+//			exit(0);//退出
+//			break;
+//		default:
+//			printf("Wrong choice! Please re-select-->\n");
+//			Sleep(1500);
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
 
-int main()
-{
-	int input = 0;
-	srand((unsigned int)time(NULL));
-	do
-	{
-		menu();
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			printf("Enter the game...\n");
-			Sleep(1000);
-			game();
-			break;
-		case 2:
-			printf("Exit the game...\n");
-			Sleep(1000);
-			exit(0);//退出
-			break;
-		default:
-			printf("Wrong choice! Please re-select-->\n");
-			Sleep(1500);
-			break;
-		}
-	} while (input);
-	return 0;
-}
+
+//递归实现一个reverse_string 将字符串逆序--不只是逆序打印！
+//思路：
+//元素下标 0 1 2 3 4 5 
+//我们只需要让0和5，4和1，2和3对应交换即可 那么我们可以设置一个函数用来交换传递过来的参数所对应的数组中的字符
+//而当第一次交换完0和5之后让它继续递归调用 左参数+1，右参数-1，即可交换1，4和2，3，直到左参数>=右参数时停止递归并返回主函数
+//总结就是，限制条件：左参数<右参数		逼近方式：每次递归调用左参数-1，右参数+1；
+//递归实现:
+//static reverse_string(char arr[],int left,size_t right)
+//{
+//	char temp = 0;
+//	temp = arr[left];
+//	arr[left] = arr[right];
+//	arr[right] = temp;
+//	if (left<right)
+//	{
+//		reverse_string(arr,left+1,right-1);
+//	}
+//}
+//非递归实现：
+//static reverse_string(char arr[], int left, size_t right)
+//{
+//	char temp = 0;
+//	while (left<right)
+//	{
+//		temp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = temp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	size_t sz = sizeof(arr) / sizeof(arr[0]);//勿忘:sizeof不能放函数中计算
+//	//这里用strlen也可，但是换成strlen-1，因为\0不计入字长
+//	reverse_string(arr, 0, sz - 2);
+//	for (int i = 0; i < sz-1; i++)
+//	{
+//		printf("%c", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//写一个递归函数 DigitSum 使其能计算数位之和
+//例如: 输入非负整数 1234 计算出 1+2+3+4=11并输出在控制台
+//思路：
+//非负整数-->unsigned int 所以可以用size_t来定义变量
+//1+2+3+4即是4+3+2+1 容易知道 取末尾数字采用%10即可 所以容易发现
+// x	   x%10
+//1234		4
+//123		3
+//12		2
+//1			1
+//0			0
+//显然x并不需要递归深入到0 只是到个位数就可以了 所以限制条件可设置为x>9 逼近方式自然是x/10
+//而后向上回归 每次回归都加上x%10(底层回归时返回x本身即可)作为整体返回值 这就实现了1+2+3+4并且最后到达顶层时又返回了主函数
+//注意 哪怕x就是个位数也无所谓 直接作底层回归 返回本身到主函数
+//size_t DigitSum(size_t x)
+//{
+//	if (x>9)
+//	{
+//		return DigitSum(x / 10)+x%10;
+//	}
+//	return x;
+//}
+//int main()
+//{
+//	size_t x= 0;
+//	scanf("%zu", &x);
+//	size_t ret =DigitSum(x);
+//	printf("%zu", ret);
+//	return 0;
+//}
 
